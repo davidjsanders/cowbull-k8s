2141bd5c4aa3757c86af001bb9fd4ddb2606ee1c
David Sanders <dsanderscanada@gmail.com>
2019-07-20 14:48:57 -0400

    Add error checking

--- scripts/load.sh
+++ scripts/load.sh
@@ -115,6 +115,10 @@ else
             s/\${LBIP}/'"$LBIP"'/g
         ' $file |
         kubectl apply -f $file &> /dev/null
+        if [ "$?" != "0" ]
+        then
+            short_banner "There was an error applying $file"
+        fi
         echo
     done
 fi
