e717dbfbab2c8a5cdc433f37b2adc3719de9bc98
David Sanders <dsanderscanada@gmail.com>
2019-07-20 14:53:34 -0400

    Update load process

--- scripts/load.sh
+++ scripts/load.sh
@@ -113,12 +113,11 @@ else
             s/\${LBIP}/'"$LBIP"'/g;
             s/\${STORAGE_CLASS}/local-storage/g;
             s/\${LBIP}/'"$LBIP"'/g
-        ' $file |
-        kubectl apply -f $file &> /dev/null
-        # if [ "$?" != "0" ]
-        # then
-        #     short_banner "There was an error applying $file"
-        # fi
+        ' $file | kubectl apply -f $file &> /dev/null
+        if [ "$?" != "0" ]
+        then
+            short_banner "There was an error applying $file"
+        fi
         echo
     done
 fi
