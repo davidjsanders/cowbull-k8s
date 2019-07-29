8fa2d96a29ffcb4f2af6c57723b6a71c49920082
David Sanders <dsanderscanada@gmail.com>
2019-07-28 21:03:35 -0400

    Remove debug and strings in getopt

--- scripts/get-options.sh
+++ scripts/get-options.sh
@@ -5,11 +5,11 @@ options=$(
         -o "s:t:l:c:v:w:" \
         -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" \
         -n "parse-options" \
-        -- "$args"
+        -- $args
 )
 ret_stat="$?"
-short_banner "Exit status from getopt: $ret_stat"
-exit 1
+#short_banner "Exit status from getopt: $ret_stat"
+
 if [ "$ret_stat" != "0" ]
 then 
     short_banner "Incorrect options provided; exit code: $ret_stat"
