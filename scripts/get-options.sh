94403ac37725c35766cf6a3b033ef24d0a3d5adf
David Sanders <dsanderscanada@gmail.com>
2019-07-28 20:44:31 -0400

    Update get-options.sh

--- scripts/get-options.sh
+++ scripts/get-options.sh
@@ -1,8 +1,5 @@
 # Call getopt to validate the provided input. 
-options=$(
-    getopt -o "s:t:l:c:v:w:" 
-    -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" -- "$@"
-)
+options=$(getopt -o "s:t:l:c:v:w:" -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" -- "$@")
 if [ $? -eq 0 ] || { 
     short_banner "Incorrect options provided"
     usage
