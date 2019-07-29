d499c7a4491675120f83231b377d1044870f8f2a
David Sanders <dsanderscanada@gmail.com>
2019-07-28 20:46:09 -0400

    Fix error checking in get-options

--- scripts/get-options.sh
+++ scripts/get-options.sh
@@ -1,7 +1,8 @@
 # Call getopt to validate the provided input. 
 options=$(getopt -o "s:t:l:c:v:w:" -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" -- "$args")
-if [ $? -eq 0 ] || { 
+if [ $? -eq 0 ]
+then 
     short_banner "Incorrect options provided"
     usage
     exit 1
-}
+fi
