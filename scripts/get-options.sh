9d0568a5bd478cc96c6cf6e6de19e41810fc3b16
David Sanders <dsanderscanada@gmail.com>
2019-07-28 20:47:51 -0400

    Update get-options

--- scripts/get-options.sh
+++ scripts/get-options.sh
@@ -1,5 +1,11 @@
 # Call getopt to validate the provided input. 
-options=$(getopt -o "s:t:l:c:v:w:" -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" -- "$args")
+echo "Arguments passed --> $args"
+options=$(
+    getopt 
+        -o "s:t:l:c:v:w:" 
+        -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" 
+        -- "$args"
+)
 if [ $? -eq 0 ]
 then 
     short_banner "Incorrect options provided"
