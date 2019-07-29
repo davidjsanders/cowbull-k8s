f2cfcae2ddb9ddbf927b1e4d1a28950e4caa95ac
David Sanders <dsanderscanada@gmail.com>
2019-07-28 20:57:38 -0400

    Debugging get-options

--- scripts/get-options.sh
+++ scripts/get-options.sh
@@ -4,10 +4,12 @@ options=$(
     getopt \
         -o "s:t:l:c:v:w:" \
         -l "load,delete,source:,target:,lbip:,storage-class:,cowbull-version:,webapp-version:" \
+        -n "parse-options" \
         -- "$args"
 )
 ret_stat="$?"
 short_banner "Exit status from getopt: $ret_stat"
+exit 1
 if [ "$ret_stat" != "0" ]
 then 
     short_banner "Incorrect options provided; exit code: $ret_stat"
