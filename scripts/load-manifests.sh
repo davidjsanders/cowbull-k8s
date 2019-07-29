b0e61b53ae53707a607f4142e052423ae76bc0b0
David Sanders <dsanderscanada@gmail.com>
2019-07-28 21:19:17 -0400

    Add delete message

--- scripts/load-manifests.sh
+++ scripts/load-manifests.sh
@@ -24,3 +24,8 @@ else
         fi
     done
 fi
+if [ "$action" == "delete.sh" ]
+then
+    short_banner "Remember the namespace and configmaps have not been deleted!"
+    short_banner "If required, delete them with: kubectl delete namespaces cowbull"
+fi
